89d6721cbd1c5850ab3c1414204c57f7b0bac4f7
Nursandy Ihksan <nursandyihksan97@gmail.com>
2022-12-29 17:23:26 +0700

    package math di golang

---
+++ other/41.math.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	fmt.Println(math.Round(14.53))
+	fmt.Println(math.Floor(14.53))
+	fmt.Println(math.Ceil(14.53))
+	fmt.Println(math.Max(10, 20))
+	fmt.Println(math.Min(10, 20))
+}
